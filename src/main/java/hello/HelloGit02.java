8cefc4b749d4688103a7df981849161abb3ab983
cait <cait@bairuitech.com>
2019-08-30 13:42:18 +0800

    第二次提交  hello git2

---
+++ src/main/java/hello/HelloGit02.java
@@ -0,0 +1,15 @@
+package hello;
+
+import javax.sound.midi.Soundbank;
+
+/**
+ * @Classname HelloGit02
+ * @Description TODO
+ * @Date 2019/8/30 13:41
+ * @Created by caitao
+ */
+public class HelloGit02 {
+    public static void main(String[] args) {
+        System.out.println("hello git 2");
+    }
+}
