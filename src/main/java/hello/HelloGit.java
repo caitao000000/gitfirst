8c5565a696fb15040576acbd7e8f6026f1aebd63
cait <cait@bairuitech.com>
2019-08-30 09:37:02 +0800

    第一次提交  hello git

---
+++ src/main/java/hello/HelloGit.java
@@ -0,0 +1,13 @@
+package hello;
+
+/**
+ * @Classname HelloGit
+ * @Description TODO
+ * @Date 2019/8/30 9:16
+ * @Created by caitao
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git ");
+    }
+}
